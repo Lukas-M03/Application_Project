fb3ded83a9fb6dd9c4cd260f233297f85b812691
Deimantas Merkevicius <Merkevicius-D@ulster.ac.uk>
2023-11-18 23:34:11 +0000

    Added fire base to our application

--- app/src/main/java/com/example/application_project/MainActivity.java
+++ app/src/main/java/com/example/application_project/MainActivity.java
@@ -1,14 +1,111 @@
 package com.example.application_project;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    Button SignOut;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        createSignInIntent();
+
+        SignOut = (Button)findViewById(R.id.btnSignout);
+        SignOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {signOut();}
+        });
+
+
+    }
+
+
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+
+    public void createSignInIntent()
+    {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.AnonymousBuilder().build());
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+
+    public void onSignInResult(FirebaseAuthUIAuthenticationResult result){
+        IdpResponse response = result.getIdpResponse();
+
+        if(result.getResultCode() == RESULT_OK){
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Toast.makeText(this, user.getEmail(), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Failed Login", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void signOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        createSignInIntent();
+                    }
+                });
     }
 }
